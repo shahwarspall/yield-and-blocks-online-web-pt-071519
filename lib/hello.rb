d66cec2cf188d65dc264c48dfbad630e872c41f3
Shahwar Spall <shevy.spall@gmail.com>
2019-07-21 05:35:32 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,7 @@
 
 def hello_t(array)
-  i = 0
+  if block_given?   
+    i = 0
  
   while i < array.length
     yield array[i]
@@ -8,6 +9,7 @@ def hello_t(array)
     
   end
   array
+
 end
 
 # call your method here!
